62df1f5cc2c44a1734cbb07fe44709865c72fbed
Ethan Catzel <ethancatzel@gmail.com>
2020-01-12 22:58:06 +1100

    Updates

---
+++ precache-manifest.1deb16340d1ad29980f9e0cb43268125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b77d90fda886ed8670a5f0c12943ecb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "3aef38928d8cae56cfcb",
+    "url": "/todo/static/css/main.71a28fa7.chunk.css"
+  },
+  {
+    "revision": "ca22a13a050977c693c7",
+    "url": "/todo/static/js/2.28cfa760.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo/static/js/2.28cfa760.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aef38928d8cae56cfcb",
+    "url": "/todo/static/js/main.15839c5e.chunk.js"
+  },
+  {
+    "revision": "9352676d6f096591a029",
+    "url": "/todo/static/js/runtime-main.96e548c6.js"
+  }
+]);
